86bbed54bbc986ecd6c9cd487a1eccca3ecdfdb9
aperezm <aperezm@e968f4be-1634-4921-b96b-e3dd0a78059a>
2011-02-08 15:11:00 +0000

    Refactoring modules to 1.3

--- tables.php
+++ tables.php
@@ -1,16 +1,4 @@
 <?php
-
-/**
- * PostNuke Application Framework
- *
- * @copyright (c) 2002, PostNuke Development Team
- * @link http://www.postnuke.com
- * @version $Id: pntables.php 22139 2007-06-01 10:57:16Z markwest $
- * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
- * @package PostNuke_Value_Addons
- * @subpackage Forums
- */
-
 /**
  * Define module tables
  * @author Albert Pérez Monfort (aperezm@xtec.cat)
@@ -25,26 +13,26 @@ function IWforums_tables() {
 
     //fields
     $tables['IWforums_definition_column'] = array('fid' => 'iw_fid',
-                                           'nom_forum' => 'iw_nom_forum',
-                                           'descriu' => 'iw_descriu',
-                                           'actiu' => 'iw_actiu',
-                                           'adjunts' => 'iw_adjunts',
-                                           'grup' => 'iw_grup',
-                                           'mod' => 'iw_mod',
-                                           'observacions' => 'iw_observacions',
-                                           'msgDelTime' => 'iw_msgDelTime',
-                                           'msgEditTime' => 'iw_msgEditTime');
+                                                  'nom_forum' => 'iw_nom_forum',
+                                                  'descriu' => 'iw_descriu',
+                                                  'actiu' => 'iw_actiu',
+                                                  'adjunts' => 'iw_adjunts',
+                                                  'grup' => 'iw_grup',
+                                                  'mod' => 'iw_mod',
+                                                  'observacions' => 'iw_observacions',
+                                                  'msgDelTime' => 'iw_msgDelTime',
+                                                  'msgEditTime' => 'iw_msgEditTime');
 
     $tables['IWforums_definition_column_def'] = array('fid' => "I NOTNULL AUTO PRIMARY",
-                                               'nom_forum' => "C(50) NOTNULL DEFAULT ''",
-                                               'descriu' => "C(255) NOTNULL DEFAULT ''",
-                                               'actiu' => "I(1) NOTNULL DEFAULT '0'",
-                                               'adjunts' => "I(1) NOTNULL DEFAULT '0'",
-                                               'grup' => "C(255) NOTNULL DEFAULT ''",
-                                               'mod' => "C(255) NOTNULL DEFAULT ''",
-                                               'observacions' => "C(255) NOTNULL DEFAULT ''",
-                                               'msgDelTime' => "C(3) NOTNULL DEFAULT '0'",
-                                               'msgEditTime' => "C(3) NOTNULL DEFAULT '0'");
+                                                      'nom_forum' => "C(50) NOTNULL DEFAULT ''",
+                                                      'descriu' => "C(255) NOTNULL DEFAULT ''",
+                                                      'actiu' => "I(1) NOTNULL DEFAULT '0'",
+                                                      'adjunts' => "I(1) NOTNULL DEFAULT '0'",
+                                                      'grup' => "C(255) NOTNULL DEFAULT ''",
+                                                      'mod' => "C(255) NOTNULL DEFAULT ''",
+                                                      'observacions' => "C(255) NOTNULL DEFAULT ''",
+                                                      'msgDelTime' => "C(3) NOTNULL DEFAULT '0'",
+                                                      'msgEditTime' => "C(3) NOTNULL DEFAULT '0'");
 
     ObjectUtil::addStandardFieldsToTableDefinition($tables['IWforums_definition_column'], 'pn_');
     ObjectUtil::addStandardFieldsToTableDataDefinition($tables['IWforums_definition_column_def'], 'iw_');
